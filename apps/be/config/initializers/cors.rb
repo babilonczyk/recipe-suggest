367011a17759f24d7830d6073de754a8bfe27133
Jan Piotrzkowski <piotrzkowski.jan@gmail.com>
2025-06-03 22:22:32 +0200

    Fix cors initializer

--- apps/be/config/initializers/cors.rb
+++ apps/be/config/initializers/cors.rb
@@ -14,7 +14,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource "/api/*",
       headers: :any,
-      methods: [ :get, :post, :patch, :put, :delete, :options ],
-      #expose: [ "Authorization", "X-API-Key" ]
+      methods: [ :get, :post, :patch, :put, :delete, :options ]
   end
 end
