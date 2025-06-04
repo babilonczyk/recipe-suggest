02af7e839732e1c3d3b5808093ed1608dec35a65
Jan Piotrzkowski <piotrzkowski.jan@gmail.com>
2025-06-04 19:18:30 +0200

    Set up cors

--- apps/be/config/initializers/cors.rb
+++ apps/be/config/initializers/cors.rb
@@ -7,9 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins = [ "http://localhost:5000", "https://recipe-suggest-web-lac.vercel.app/" ]
-
-    origins origins
+    origins "http://localhost:5000", "https://recipe-suggest-web-lac.vercel.app"
 
     resource "/api/*",
       headers: :any,
